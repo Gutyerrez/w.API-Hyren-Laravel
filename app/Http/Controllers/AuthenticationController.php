9dd05bc7bea98db28d4af11d8ddcfa79e9a92cfa
Gutyerrez <srgutyerrez@gmail.com>
2020-10-07 03:00:43 -0300

    dev

--- app/Http/Controllers/AuthenticationController.php
+++ app/Http/Controllers/AuthenticationController.php
@@ -7,21 +7,10 @@ use App\Http\Middleware\AuthenticationMiddleware;
 
 class AuthenticationController extends Controller
 {
-    const KEY = 'HYREN-APPLICATION-8768-144534578-48788-12345787';
 
     public static function store(Request $request)
     {
-        $origin = $request->input('origin');
-
-        if (empty($origin)) {
-            return response()->json([
-                'status' => 'fail',
-                'message' => 'Please inform origin'
-            ], 400);
-        }
-
         $payload = json_encode([
-            'origin' => $origin,
             'time' => microtime(true),
             'duration' => 60000
         ]);
@@ -33,4 +22,5 @@ class AuthenticationController extends Controller
             'access_token' => $encrypted
         ], 200);
     }
+
 }
